41e3b32da39f246856a5bfb1b884a431ee4ae927
Christopher Pedersen <chrisaped@gmail.com>
2021-09-08 08:05:03 +0200

    created AlpacaDataStream class, added dotenv

---
+++ AlpacaDataStream.js
@@ -0,0 +1,66 @@
+"use strict";
+
+require('dotenv').config()
+const Alpaca = require("@alpacahq/alpaca-trade-api");
+
+class AlpacaDataStream {
+  constructor({ apiKey, secretKey, feed }) {
+    this.alpaca = new Alpaca({
+      keyId: apiKey,
+      secretKey,
+      feed,
+    });
+
+    const socket = this.alpaca.data_stream_v2;
+
+    socket.onConnect(function () {
+      console.log("Connected");
+      socket.subscribeForQuotes(["AAPL"]);
+      socket.subscribeForTrades(["FB"]);
+      socket.subscribeForBars(["SPY"]);
+      socket.subscribeForStatuses(["*"]);
+    });
+
+    socket.onError((err) => {
+      console.log(err);
+    });
+
+    socket.onStockTrade((trade) => {
+      console.log(trade);
+    });
+
+    socket.onStockQuote((quote) => {
+      console.log(quote);
+    });
+
+    socket.onStockBar((bar) => {
+      console.log(bar);
+    });
+
+    socket.onStatuses((s) => {
+      console.log(s);
+    });
+
+    socket.onStateChange((state) => {
+      console.log(state);
+    });
+
+    socket.onDisconnect(() => {
+      console.log("Disconnected");
+    });
+
+    socket.connect();
+
+    // disconnect after 5 seconds
+    setTimeout(() => {
+      socket.disconnect();
+    }, 5000);
+  }
+}
+
+let stream = new AlpacaDataStream({
+  apiKey: process.env.ALPACA_API_KEY,
+  secretKey: process.env.ALPACA_API_SECRET,
+  feed: "sip",
+  paper: true,
+})
