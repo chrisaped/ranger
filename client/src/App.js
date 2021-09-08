9a26e8a2da71c90dca9cac39f858807f66380146
Christopher Pedersen <chrisaped@gmail.com>
2021-09-08 15:16:43 +0200

    socket connection working with socket.io

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
+import { 
+  initiateSocketConnection,
+  disconnectSocket
+} from "./socket.io.service";
+
 export default function App() {
+  useEffect(() => {
+    initiateSocketConnection();
+    return () => {
+      disconnectSocket();
+    }    
+  }, []);
+
   async function callAlpacaDataStream() {
-    const response = await fetch('/alpaca_data_stream', { method: 'POST' })
-    console.log('here is the response', response)
+    const response = await fetch('/alpaca_data_stream', { method: 'POST' });
+    console.log('here is the response', response);
   }
 
   return (
--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
-const alpacaDataStream = require('./AlpacaDataStream').AlpacaDataStream
-
-app.listen(port, () => console.log(`Listening on port ${port}`))
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const port = process.env.PORT || 5000;
+const io = require('socket.io')(http, {
+  cors: {
+    origins: ['http://localhost:3000']
+  }
+});
+const alpacaDataStream = require('./AlpacaDataStream').AlpacaDataStream;
 
 app.post('/alpaca_data_stream', (req, res) => {
-  console.log('here is the request', req)
-  let stream = new alpacaDataStream(['GME'])
-  console.log('it works!', stream)
-  res.send({ dataStream: stream })
+  console.log('here is the request', req);
+  let stream = new alpacaDataStream(['GME']);
+  console.log('it works!', stream);
+  res.send({ dataStream: stream });
 })
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(port, () => console.log(`Listening on port ${port}`));
