4dff2c2d0e43a017e85f77b734ea5248d2558f97
Christopher Pedersen <chrisaped@gmail.com>
2021-09-09 17:06:02 +0200

    Revert "added redux example, working"
    This reverts commit 625da376410e21df993dff6a2a6d448d15292eaf.

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { store } from './configure-store';
-// import App from './App';
-import { Container } from './counter/container';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      {/* <App /> */}
-      <Container />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
