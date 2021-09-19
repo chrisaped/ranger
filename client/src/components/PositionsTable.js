985ef43ce5e1e2a7f24ff1eac3d8b87e4161c1a8
Christopher Pedersen <chrisaped@gmail.com>
2021-09-19 18:14:47 +0200

    more PositionsTable styling

--- client/src/components/PositionsTable.js
+++ client/src/components/PositionsTable.js
@@ -70,20 +70,6 @@ export default function PositionsTable({ socket, positions, orders, quotes }) {
   
   return (
     <table className="table table-bordered">
-      <thead className="table-dark">
-        <tr>
-          <th>Symbol</th>
-          <th>Side</th>
-          <th>Current Price</th>
-          <th>Entry Price</th>
-          <th>Shares</th>
-          <th>Cost</th>
-          <th>P/L</th>
-          <th>Status</th>
-          <th colSpan="1">Actions</th>
-        </tr>
-      </thead>
-      <tbody>
       {positions.map((positionObj, index) => {
         const {
           symbol,
@@ -106,69 +92,82 @@ export default function PositionsTable({ socket, positions, orders, quotes }) {
 
         return (
           <>
-            <tr 
-              key={index} 
-              className={
-                isInProfit(unrealized_intraday_pl) ? "table-success" : "table-danger"
-              }
-            >
-              <td><strong>{symbol}</strong></td>
-              <td>{side.toUpperCase()}</td>
-              <td><strong>{currentPrice}</strong></td>
-              <td>{avg_entry_price}</td>
-              <td>{qty}</td>
-              <td>${cost}</td>
-              <td>{unrealized_intraday_pl}</td>
-              <td>{parentOrderStatus}</td>
-              <td>
-                <button 
-                  className="btn btn-dark m-2" 
-                  onClick={() => createOrder(orderSellObject)}
-                  // disabled= if I dont have enough money
-                >
-                  Sell
-                </button>
-              </td>            
-            </tr>
-            <tr>
-              <td className="bg-dark text-white fw-bold">
-                Take Profit
-              </td>
-              <td className="bg-secondary text-white">Price</td>
-              <td>{targetPrice}</td>
-              <td className="bg-secondary text-white">Status</td>
-              <td>{targetOrderStatus}</td>
-              <td className="bg-light" colSpan="4">
-                <button 
-                  className="btn btn-outline-dark btn-sm m-2" 
-                >
-                  Cancel
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <td className="bg-dark text-white fw-bold">
-                Stop Loss
-              </td>
-              <td className="bg-secondary text-white">Price</td>
-              <td>{stopPrice}</td>
-              <td className="bg-secondary text-white">Status</td>
-              <td>{stopOrderStatus}</td>
-              <td className="bg-light" colSpan="4">
-                <button 
-                  className="btn btn-outline-dark btn-sm m-2" 
-                >
-                  Cancel
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <td className="bg-dark" colSpan="9"></td>
-            </tr>
+            <thead className="table-dark">
+              <tr>
+                <th>Symbol</th>
+                <th>Side</th>
+                <th>Current Price</th>
+                <th>Entry Price</th>
+                <th>Shares</th>
+                <th>Cost</th>
+                <th>P/L</th>
+                <th>Status</th>
+                <th colSpan="1">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr 
+                key={index} 
+                className={
+                  isInProfit(unrealized_intraday_pl) ? "table-success" : "table-danger"
+                }
+              >
+                <td><strong>{symbol}</strong></td>
+                <td>{side.toUpperCase()}</td>
+                <td><strong>{currentPrice}</strong></td>
+                <td>{avg_entry_price}</td>
+                <td>{qty} shares</td>
+                <td>${cost}</td>
+                <td>${unrealized_intraday_pl}</td>
+                <td>{parentOrderStatus}</td>
+                <td>
+                  <button 
+                    className="btn btn-dark m-2" 
+                    onClick={() => createOrder(orderSellObject)}
+                    // disabled= if I dont have enough money
+                  >
+                    Sell
+                  </button>
+                </td>            
+              </tr>
+              <tr>
+                <td className="bg-dark text-white fw-bold">
+                  Take Profit
+                </td>
+                <td className="bg-secondary text-white">Price</td>
+                <td>{targetPrice}</td>
+                <td className="bg-secondary text-white">Status</td>
+                <td>{targetOrderStatus}</td>
+                <td className="bg-light" colSpan="4">
+                  <button 
+                    className="btn btn-outline-dark btn-sm m-2"
+                    disabled={targetOrderStatus === "filled" || "canceled"}
+                  >
+                    Cancel
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td className="bg-dark text-white fw-bold">
+                  Stop Loss
+                </td>
+                <td className="bg-secondary text-white">Price</td>
+                <td>{stopPrice}</td>
+                <td className="bg-secondary text-white">Status</td>
+                <td>{stopOrderStatus}</td>
+                <td className="bg-light" colSpan="4">
+                  <button 
+                    className="btn btn-outline-dark btn-sm m-2"
+                    disabled={stopOrderStatus === "filled" || "canceled"}
+                  >
+                    Cancel
+                  </button>
+                </td>
+              </tr>
+            </tbody>
           </>
         );
       })}
-      </tbody>
     </table>
   );
 }
