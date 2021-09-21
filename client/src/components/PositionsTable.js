d13fe51e6e006862228dd3660a6709a13e31529b
Christopher Pedersen <chrisaped@gmail.com>
2021-09-21 17:26:29 +0200

    proper indent

--- client/src/components/PositionsTable.js
+++ client/src/components/PositionsTable.js
@@ -106,57 +106,57 @@ export default function PositionsTable({ socket, positions, orders, quotes }) {
       
       return (
         <table className="table table-bordered" key={clientOrderId}>
-            <thead className="table-dark">
-              <tr>
-                <th>Symbol</th>
-                <th>Side</th>
-                <th>Current Price</th>
-                <th>Entry Price</th>
-                <th>Target Price</th>
-                <th>Stop Loss</th>
-                <th>Shares</th>
-                <th>Cost</th>
-                <th>P/L</th>
-                <th colSpan="2">Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr 
-                key={index} 
-                className={
-                  isInProfit(profitOrLoss) ? "table-success" : "table-danger"
-                }
-              >
-                <td><strong>{symbol}</strong></td>
-                <td>{side.toUpperCase()}</td>
-                <td className="bg-warning"><strong>{currentPrice}</strong></td>
-                <td className="bg-info">{entryPrice}</td>
-                <td className={hasNoBracketOrder ? 'bg-secondary' : "bg-success text-white"}>{targetPrice}</td>
-                <td className={hasNoBracketOrder ? 'bg-secondary' : "bg-danger text-white"}>{stopPrice}</td>
-                <td>{qty} shares</td>
-                <td>${cost}</td>
-                <td>${profitOrLoss}</td>
-                <td>
-                  <SpinnerButton
-                    socket={socket}
-                    buttonClass="btn btn-dark m-2"
-                    buttonText="Cancel Bracket"
-                    buttonDisabled={hasNoBracketOrder}
-                    onClickFunction={cancelBracket}
-                  />
-                </td>                
-                <td>
-                  <SpinnerButton 
-                    socket={socket}
-                    buttonClass="btn btn-dark m-2"
-                    buttonText="Sell"
-                    buttonDisabled={!hasNoBracketOrder}
-                    onClickFunction={submitOrder}
-                  />
-                </td>            
-              </tr>
-            </tbody>
-            </table>
+          <thead className="table-dark">
+            <tr>
+              <th>Symbol</th>
+              <th>Side</th>
+              <th>Current Price</th>
+              <th>Entry Price</th>
+              <th>Target Price</th>
+              <th>Stop Loss</th>
+              <th>Shares</th>
+              <th>Cost</th>
+              <th>P/L</th>
+              <th colSpan="2">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr 
+              key={index} 
+              className={
+                isInProfit(profitOrLoss) ? "table-success" : "table-danger"
+              }
+            >
+              <td><strong>{symbol}</strong></td>
+              <td>{side.toUpperCase()}</td>
+              <td className="bg-warning"><strong>{currentPrice}</strong></td>
+              <td className="bg-info">{entryPrice}</td>
+              <td className={hasNoBracketOrder ? 'bg-secondary' : "bg-success text-white"}>{targetPrice}</td>
+              <td className={hasNoBracketOrder ? 'bg-secondary' : "bg-danger text-white"}>{stopPrice}</td>
+              <td>{qty} shares</td>
+              <td>${cost}</td>
+              <td>${profitOrLoss}</td>
+              <td>
+                <SpinnerButton
+                  socket={socket}
+                  buttonClass="btn btn-dark m-2"
+                  buttonText="Cancel Bracket"
+                  buttonDisabled={hasNoBracketOrder}
+                  onClickFunction={cancelBracket}
+                />
+              </td>                
+              <td>
+                <SpinnerButton 
+                  socket={socket}
+                  buttonClass="btn btn-dark m-2"
+                  buttonText="Sell"
+                  buttonDisabled={!hasNoBracketOrder}
+                  onClickFunction={submitOrder}
+                />
+              </td>            
+            </tr>
+          </tbody>
+        </table>
         );
       })}
       </div>
