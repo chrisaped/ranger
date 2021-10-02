7d09b3a4f6c16ce1f2ca571e5ea2fa3cce0b5b18
Christopher Pedersen <chrisaped@gmail.com>
2021-10-02 12:14:34 +0200

    created PositionsTableRowData

--- client/src/components/PositionsTable.js
+++ client/src/components/PositionsTable.js
@@ -1,15 +1,9 @@
-import { displayPrice } from "../shared/formatting";
+import PositionsTableRowData from "./PositionsTableRowData";
 import { calculateProfitLoss } from "../shared/calculations";
-import { 
-  createMarketOrder,
-  createOrder,
-  cancelOrder,
-  extractBracketOrderInfo
-} from "../shared/orders";
-import SpinnerButton from "./SpinnerButton";
+import { extractBracketOrderInfo } from "../shared/orders";
 
 export default function PositionsTable({ socket, positions, orders, quotes }) {
-  const rowClassName = (profitOrLoss) => {
+  const getRowClassName = (profitOrLoss) => {
     let rowClass;
     if (profitOrLoss > 0) {
       rowClass = "table-success";
@@ -53,47 +47,26 @@ export default function PositionsTable({ socket, positions, orders, quotes }) {
           targetOrderId,
           hasLegs
         } = extractBracketOrderInfo(symbol, quantity, avg_entry_price, orders);
-        const currentPrice = displayPrice(quotes[symbol]);
-        const entryPrice = displayPrice(avg_entry_price);
-        const marketOrder = createMarketOrder(symbol, quantity, side);
-        const submitOrder = () => createOrder(socket, marketOrder);
-        const cost = Math.round(entryPrice * quantity).toLocaleString();
-        const cancelBracket = () => cancelOrder(socket, targetOrderId);
-        const profitOrLoss = (calculateProfitLoss(currentPrice, entryPrice, quantity, side)).toFixed(2);
-        const hasNoBracketOrder = !hasLegs || (targetOrderStatus === "canceled");
+        const price = quotes[symbol];
+        const profitOrLoss = (calculateProfitLoss(price, avg_entry_price, quantity, side)).toFixed(2);
+        const rowClassName = getRowClassName(profitOrLoss);
+
         return (
-          <tr className={rowClassName(profitOrLoss)} key={symbol}>
-            <td><strong>{symbol}</strong></td>
-            <td>{side.toUpperCase()}</td>
-            <td className="bg-info">{entryPrice}</td>
-            <td className="bg-warning"><strong>{currentPrice}</strong></td>
-            <td className={hasNoBracketOrder ? 'bg-secondary' : "bg-success text-white"}>{targetPrice}</td>
-            <td className={hasNoBracketOrder ? 'bg-secondary' : "bg-danger text-white"}>{stopPrice}</td>
-            <td>${profitOrLoss}</td>
-            <td>{quantity} shares</td>
-            <td>${cost}</td>
-            <td>
-            {hasNoBracketOrder ? (
-              <SpinnerButton 
-                socket={socket}
-                buttonClass="btn btn-dark"
-                buttonText={side === "long" ? "Sell" : "Buy"}
-                buttonDisabled={!hasNoBracketOrder}
-                onClickFunction={submitOrder}
-                symbol={symbol}
-              />
-            ):(
-              <SpinnerButton
-                socket={socket}
-                buttonClass="btn btn-dark"
-                buttonText="Cancel Bracket"
-                buttonDisabled={hasNoBracketOrder}
-                onClickFunction={cancelBracket}
-                orderId={targetOrderId}
-                symbol={symbol}
-              />              
-            )}
-            </td>
+          <tr className={rowClassName} key={symbol}>
+            <PositionsTableRowData 
+              socket={socket}
+              symbol={symbol}
+              side={side}
+              avgEntryPrice={avg_entry_price}
+              price={price}
+              hasLegs={hasLegs}
+              targetOrderStatus={targetOrderStatus}
+              targetPrice={targetPrice}
+              stopPrice={stopPrice}
+              profitOrLoss={profitOrLoss}
+              quantity={quantity}
+              targetOrderId={targetOrderId}
+            />
           </tr>
         );
       })}
---
+++ client/src/components/PositionsTableRowData.js
@@ -0,0 +1,70 @@
+import SpinnerButton from "./SpinnerButton";
+import { displayPrice } from "../shared/formatting";
+import { 
+  createMarketOrder,
+  createOrder,
+  cancelOrder
+} from "../shared/orders";
+
+export default function PositionsTableRowData({
+  socket,
+  symbol,
+  side,
+  avgEntryPrice,
+  price,
+  hasLegs,
+  targetOrderStatus,
+  targetPrice,
+  stopPrice,
+  profitOrLoss,
+  quantity,
+  targetOrderId
+}) {
+  const sideInCaps = side.toUpperCase();
+  const entryPrice = displayPrice(avgEntryPrice);
+  const currentPrice = displayPrice(price);
+  const hasNoBracketOrder = !hasLegs || (targetOrderStatus === "canceled");
+  const targetPriceClassName = hasNoBracketOrder ? 'bg-secondary' : "bg-success text-white";
+  const stopPriceClassName = hasNoBracketOrder ? 'bg-secondary' : "bg-danger text-white";
+  const cost = Math.round(avgEntryPrice * quantity).toLocaleString();
+  const marketOrder = createMarketOrder(symbol, quantity, side);
+  const submitOrder = () => createOrder(socket, marketOrder);
+  const cancelBracket = () => cancelOrder(socket, targetOrderId);
+  const orderButtonText = side === "long" ? "Sell" : "Buy";
+
+  return (
+    <>
+      <td><strong>{symbol}</strong></td>
+      <td>{sideInCaps}</td>
+      <td className="bg-info">{entryPrice}</td>
+      <td className="bg-warning"><strong>{currentPrice}</strong></td>
+      <td className={targetPriceClassName}>{targetPrice}</td>
+      <td className={stopPriceClassName}>{stopPrice}</td>
+      <td>${profitOrLoss}</td>
+      <td>{quantity} shares</td>
+      <td>${cost}</td>
+      <td>
+      {hasNoBracketOrder ? (
+        <SpinnerButton 
+          socket={socket}
+          buttonClass="btn btn-dark"
+          buttonText={orderButtonText}
+          buttonDisabled={!hasNoBracketOrder}
+          onClickFunction={submitOrder}
+          symbol={symbol}
+        />
+      ):(
+        <SpinnerButton
+          socket={socket}
+          buttonClass="btn btn-dark"
+          buttonText="Cancel Bracket"
+          buttonDisabled={hasNoBracketOrder}
+          onClickFunction={cancelBracket}
+          orderId={targetOrderId}
+          symbol={symbol}
+        />              
+      )}
+      </td>    
+    </>
+  );
+}
