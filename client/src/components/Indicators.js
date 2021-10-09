87c02cad072b69ddde141ff49fb5a9ef066a4d3c
Christopher Pedersen <chrisaped@gmail.com>
2021-10-09 16:33:42 +0200

    added barObj data sample

--- client/src/components/Indicators.js
+++ client/src/components/Indicators.js
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 import PropTypes from 'prop-types';
 
 export default function Indicators({ socket, symbol, barObj }) {
+  // 5 min indicators needed: VWAP, 3 EMA, 8 EMA
+  // vwap does not need to be calculated since it is included in alpaca bars data
+
   const [SMA, setSMA] = useState(0);
 
   useEffect(() => {
---
+++ client/src/dataSamples/barObj.js
@@ -0,0 +1,13 @@
+export const barObj = {
+  T: 'b',
+  Symbol: 'GME',
+  OpenPrice: 172.89,
+  ClosePrice: 172.82,
+  HighPrice: 172.89,
+  LowPrice: 172.65,
+  Volume: 3591,
+  Timestamp: "2021-10-08T14:24:00.000Z",
+  n: 117,
+  vw: 172.776705
+}
+// vw is VWAP
