ea524df0ed5d60b8bcfdcfe414ccd16913555236
Christopher Pedersen <chrisaped@gmail.com>
2022-09-16 10:15:51 +0200

    not using useCallback

--- client/src/components/Watchlist.js
+++ client/src/components/Watchlist.js
@@ -1,45 +1,52 @@
 import { useEffect, useCallback } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import QuotesTable from "./QuotesTable";
 
-export default function Watchlist({ 
-  socket, 
-  quotes, 
-  setQuotes, 
-  watchlist, 
+export default function Watchlist({
+  socket,
+  quotes,
+  setQuotes,
+  watchlist,
   setWatchlist,
   tradeableAssets,
-  accountInfo
+  accountInfo,
 }) {
   useEffect(() => {
-    socket.on('getWatchlist', (symbols) => {
+    socket.on("getWatchlist", (symbols) => {
       setWatchlist(symbols);
     });
   }, [socket, setWatchlist]);
 
-  const removeFromQuotesAndWatchlist = useCallback((symbol) => {
-    const newWatchlist = watchlist.filter(watchlistSymbol => watchlistSymbol !== symbol);
-    setWatchlist(newWatchlist);
-    if (Object.keys(quotes).length > 0) {
-      const newQuotes = quotes;
-      delete newQuotes[symbol];
-      setQuotes(newQuotes);
-    }
-    socket.emit('removeFromQuotesAndWatchlist', symbol);
-  }, [quotes, setQuotes, setWatchlist, socket, watchlist]);
-
-  const removeFromWatchlist = useCallback(symbol => {
-    if (watchlist.includes(symbol)) {
-      const newWatchlist = watchlist.filter(watchlistSymbol => watchlistSymbol !== symbol);
+  const removeFromQuotesAndWatchlist = useCallback(
+    (symbol) => {
+      const newWatchlist = watchlist.filter(
+        (watchlistSymbol) => watchlistSymbol !== symbol
+      );
       setWatchlist(newWatchlist);
-      socket.emit('removeFromWatchlist', symbol);
+      if (Object.keys(quotes).length > 0) {
+        const newQuotes = quotes;
+        delete newQuotes[symbol];
+        setQuotes(newQuotes);
+      }
+      socket.emit("removeFromQuotesAndWatchlist", symbol);
+    },
+    [quotes, setQuotes, setWatchlist, socket, watchlist]
+  );
+
+  const removeFromWatchlist = (symbol) => {
+    if (watchlist.includes(symbol)) {
+      const newWatchlist = watchlist.filter(
+        (watchlistSymbol) => watchlistSymbol !== symbol
+      );
+      setWatchlist(newWatchlist);
+      socket.emit("removeFromWatchlist", symbol);
     }
-  }, [setWatchlist, socket, watchlist]);
+  };
 
   return (
     <div>
       {watchlist.length > 0 && (
-        <QuotesTable 
+        <QuotesTable
           socket={socket}
           quotes={quotes}
           removeFromQuotesAndWatchlist={removeFromQuotesAndWatchlist}
@@ -54,19 +61,19 @@ export default function Watchlist({
 }
 
 Watchlist.propTypes = {
-  socket: PropTypes.object.isRequired, 
-  quotes: PropTypes.object.isRequired, 
-  setQuotes: PropTypes.func.isRequired, 
-  watchlist: PropTypes.arrayOf(PropTypes.string).isRequired, 
+  socket: PropTypes.object.isRequired,
+  quotes: PropTypes.object.isRequired,
+  setQuotes: PropTypes.func.isRequired,
+  watchlist: PropTypes.arrayOf(PropTypes.string).isRequired,
   setWatchlist: PropTypes.func.isRequired,
   tradeableAssets: PropTypes.object.isRequired,
-  accountInfo: PropTypes.object.isRequired
+  accountInfo: PropTypes.object.isRequired,
 };
 
 Watchlist.defaultProps = {
-  socket: {}, 
-  quotes: {}, 
-  watchlist: [], 
+  socket: {},
+  quotes: {},
+  watchlist: [],
   tradeableAssets: {},
-  accountInfo: {}
+  accountInfo: {},
 };
